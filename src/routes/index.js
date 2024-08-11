dc9eccd68f80f3f8e0e60096582a77485c8f7547
dchingap <dchingap@gmail.com>
2024-08-11 13:42:52 -0500

    agregue la pagina de login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,25 @@
-import { Router } from "express"
-const router = Router()
+import { Router } from "express";
+const router = Router();
 
 //Ruta Pagina de Inicio
-router.get('/',(req,res)=> res.render('index',{title:'Ecommerce Ferreteria'}))
+router.get("/", (req, res) =>
+  res.render("index", { title: "Ecommerce Ferreteria" })
+);
 
 //Ruta Pagina de Nosotros
-router.get('/about',(req,res)=>res.render('about',{title:'Nosotros'}))
+router.get("/about", (req, res) => res.render("about", { title: "Nosotros" }));
 
 //Ruta Pagina de Contacto
-router.get('/contacto',(req,res)=>res.render('contacto',{title:'Contacto'}))
+router.get("/contacto", (req, res) =>
+  res.render("contacto", { title: "Contacto" })
+);
 
 //Ruta Pagina de tienda
-router.get('/tienda',(req,res)=>res.render('tienda',{title:'Productos Online'}))
+router.get("/tienda", (req, res) =>
+  res.render("tienda", { title: "Productos Online" })
+);
 
+//Ruta Pagina de Login
+router.get("/login", (req, res) => res.render("login", { title: "Login" }));
 
-export default router
+export default router;
